cc066b203e7f776ddabeb8f58f1686a6f1571ac9
zaka.pong <zaka.pong@yahoo.com>
2018-05-18 11:15:07 +0200

    Display the search results

--- js/movies.js
+++ js/movies.js
@@ -1,6 +1,8 @@
 
 data = '';
+var displayMovie = "";
 
+function loadData () {
 var url = "https://api.nytimes.com/svc/movies/v2/reviews/search.json";
 url += '?' + $.param({
   'api-key': "aa6b434cdfd24541a23901a5043479de"
@@ -9,34 +11,40 @@ $.ajax({
   url: url,
   method: 'GET',
 }).done(function(response) {
-    displayMovie = "";
   data = response.results;
   var dataStore = localStorage.setItem('dataStore', JSON.stringify(data));
-  for (i = 1; i < data.length; i++) {
+  displayData(data);
+
+  $('#reviews').html(displayMovie); 
+  displayMovie = "";
+
+}).fail(function(err) {
+  throw err;
+});
+}
+
+
+function displayData(infoToDisplay) {
+  for (i = 0; i < infoToDisplay.length; i++) {
     displayMovie += `<div id="reviewId" class="col-md-4">
     <div class="card mb-4 box-shadow">
-      <img class="card-img-top"  alt="Thumbnail [100%x225]" src="` + data[i].multimedia.src +`" data-holder-rendered="true">
+      <img class="card-img-top"  alt="Thumbnail [100%x225]" src="` + infoToDisplay[i].multimedia.src +`" data-holder-rendered="true">
       <div class="card-body">
-        <h2>` + data[i].display_title + `</h2>
-        <p class="card-text">` + data[i].summary_short + `</p>
+        <h2>` + infoToDisplay[i].display_title + `</h2>
+        <p class="card-text">` + infoToDisplay[i].headline + `</p>
         <div class="d-flex justify-content-between align-items-center">
           <div class="btn-group">
           <a onclick="selectedMovieReview('` + i + `')" ><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
           </div>
-            <small class="text-muted"> Rating: `+ data[i].mpaa_rating + `</small>
+            <small class="text-muted"> Rating: `+ infoToDisplay[i].mpaa_rating + `</small>
           </div>
         </div>
       </div>
     </div>`;
   }
+}
 
-  $('#reviews').html(displayMovie); 
-
-}).fail(function(err) {
-  throw err;
-});
-
-
+ 
 
 function selectedMovieReview(reviewMovieId) {
   var reviewId = sessionStorage.setItem('reviewId', reviewMovieId);
@@ -65,13 +73,43 @@ function getMovieDetails(){
 }
 
 
-function gotoMainPage () {
-	    document.getElementById("go-to-main-button").onclick = function () {
-        window.location = "index.html";
-    };
+function searchText() {
+
+  var term =document.getElementById("searchTerm").value
+  var url = "https://api.nytimes.com/svc/movies/v2/reviews/search.json";
+url += '?' + $.param({
+  'api-key': "aa6b434cdfd24541a23901a5043479de",
+  'query': term
+});
+
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(response) {
+  var updatedInfo = response.results;
+  var output = '';
+  console.log(updatedInfo);
+
+  displayData(updatedInfo);
+document.getElementById("reviews").innerHTML = "";
+
+ $('#reviews').html(displayMovie);
+ // clear review   
+ displayMovie = "";
+
+}).fail(function(err) {
+  throw err;
+});
 }
 
 
+function gotoMainPage () {
+        window.location = "index.html";
+    }
+
+$(document).ready(function (event) {
+	loadData();
+});
 
 
  
